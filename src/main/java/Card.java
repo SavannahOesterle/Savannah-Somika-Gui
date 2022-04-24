4ab1b3dc19afda96d7fbf2698fc088fc05816438
Somika Ganesh <1698891@park.edu>
2022-04-24 14:13:55 -0500

    Worked on the logic part of game. For now, I have kept win and lose range to 3 and 1 respectively. we need to figure out that.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,28 +1,55 @@
 import java.util.Random;
 
 public class Card {
-
     private int value;
     private boolean faceUp;
 
     /**
-     * Constructor that creates a random card
-     * @return an integer value between 1 - 9
+     * Constructor that Creates a Card, with 1 as the associated value and face
+     * down.
      */
-    public int Card () {
-        faceUp = false;
+    public Card() {
+        value = 0;
+        setFaceUp(false);
+    }
 
+    /**
+     * Method that creates a random card
+     *@return an integer value between 1 - 9
+     */
+    public int shuffle() {
         Random rand = new Random();
-        int value = rand.nextInt(10);
+        value = rand.nextInt(10);
         return value;
     }
 
     /**
-     * Method that flips the card
-     * Once a card is turned face up, it remains face up for the rest of the round
+     * Method that will check if the card is face up or not
+     * @return a boolean
      */
-    public void flipCard(){
-        faceUp = true;
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    /**
+     * Method that flips the card Once a card is turned face up, it remains face up
+     * for the rest of the round
+     */
+    public void setFaceUp(boolean faceUp) {
+        this.faceUp = faceUp;
+    }
+
+    /**
+     * Method that will view the value of the card.
+     * @return a integer value of card if its face is turned upside.
+     */
+    public int viewCard() {
+        if (faceUp)
+            return value;
+        else {
+            System.out.println("Card face is down. Kindly invert.");
+            return -1;
+        }
     }
 
 }
---
+++ src/main/java/GameConstants.java
@@ -0,0 +1,6 @@
+public interface GameConstants {
+    public static int WIN_POINTS = 3;
+    public static int LOSE_POINTS = 1;
+    public final static String HIGHER = "Higher";
+    public final static String LOWER = "Lower";
+}
